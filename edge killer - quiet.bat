ad48755a68f69c3f7e88795bef64954e68a86e66
ItsIceCreeperPE <56017038+Official-IceCreeperPE@users.noreply.github.com>
2022-06-06 23:02:08 +0200

    Add files via upload

---
+++ edge killer - quiet.bat
@@ -0,0 +1,3 @@
+@echo off
+taskkill /IM msedge.exe /F
+exit
---
+++ edge killer.bat
@@ -0,0 +1,8 @@
+@echo off
+taskkill /IM msedge.exe /F
+cls
+echo Microsoft Edge has been killed!
+echo Tool by ItsIceCreeperPE
+echo Press any key to exit
+pause > nul
+exit
